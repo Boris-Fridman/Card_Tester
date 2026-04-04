f4b41b12f3775d07a2e973be8f7dbc0dbe8622f8
Dag <dag@dag.dag>
2026-04-04 17:32:03 +0300

    Were created two projects: UUT_Prog - The program burnt into the card for Unit Under Test and the Comp_Prog - The program running on a computer.

---
+++ Comp_Prog/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+    
+int main(void)
+ {
+    //ansi clear screen
+    printf("\033[2J\033[H");
+    
+    //code
+    
+    return 0;
+ }
