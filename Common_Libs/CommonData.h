037d84218ef1c9fa1252b77355ee477ae55220a2
Dag <dag@dag.dag>
2026-04-04 21:52:00 +0300

    Changes to be committed: 	new file:   Common_Libs/CommonData.c 	new file:   Common_Libs/CommonData.h 	new file:   Comp_Prog/.vscode/c_cpp_properties.json 	modified:   Comp_Prog/.vscode/launch.json 	modified:   Comp_Prog/.vscode/tasks.json 	modified:   Comp_Prog/main.c 	modified:   Comp_Prog/program
    Was added the new folder with common files for common definitions and with some of them.
    Was added some testing algorithm with some comments to the program running on a computer.

---
+++ Common_Libs/CommonData.h
@@ -0,0 +1,58 @@
+#ifndef ____CommonData_h__
+#define ____CommonData_h__
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX(X, Y) ( (X) > (Y) ? (X) : (Y) )  // The macro returning the biggest from the two values.
+#define MIN(X, Y) ( (X) < (Y) ? (X) : (Y) )  // The macro returning the smallest from the two values.
+
+
+
+typedef enum __attribute__((__packed__)) PeriphType_e  // The attribute "__attribute__((__packed__))" is defined to make the enumeration to be in one byte to ensure the correct data length while sending. 
+ {
+  E_TIMER =  1,
+  E_UART  =  2,
+  E_SPI   =  4,
+  E_I2C   =  8,
+  E_ADC   = 16
+ }PeriphType_e;
+
+typedef enum __attribute__((__packed__)) TestResType_e  // The attribute "__attribute__((__packed__))" is defined to make the enumeration to be in one byte to ensure the correct data length while sending. 
+ {
+  E_TEST_SUCCEEDED =    0,
+  E_TEST_FAILED    = 0xFF
+ }TestResType_e;
+
+
+typedef struct PeriphBitField_s
+ {
+  uint8_t Timer     : 1;
+  uint8_t UART      : 1;
+  uint8_t SPI       : 1;
+  uint8_t I2c       : 1;
+  uint8_t ADC       : 1;
+  uint8_t Reserved  : 3;
+ }PeriphBitField_s;
+
+typedef struct __attribute__((packed)) TestData_s // The attribute "__attribute__((packed))" is defined to make the struct to be at the exact size as it is defined to ensure the correct data lendght while sending.
+ {
+  uint32_t Test_ID;
+  PeriphBitField_s Periph_B_F;
+  uint8_t Num_Interations;
+  uint8_t Bit_Pattern_Length;
+ }TestData_s;
+
+
+typedef struct __attribute__((packed)) TestResult_s // The attribute "__attribute__((packed))" is defined to make the struct to be at the exact size as it is defined to ensure the correct data lendght while sending.
+ {
+  uint32_t Test_ID;
+  PeriphBitField_s Periph_B_F;
+  TestResType_e TestResult;
+ }TestResult_s;
+
+
+#endif  //  ____CommonData_h__
+
--- Comp_Prog/main.c
+++ Comp_Prog/main.c
@@ -2,13 +2,33 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include "CommonData.h"
     
-int main(void)
+
+/*
+* To use the program it's needed to type the next parameters:
+* <filename> [test [t][u][s][i][a][r]] 
+* t - Timer
+* u - UART/USART
+* s - SPI
+* i - I2C
+* a - ADC
+* or to type h to see the help message.
+*/
+
+int main(int argc, char *argv[])
  {
     //ansi clear screen
     printf("\033[2J\033[H");
     
     //code
+    printf("printing arguments...\r\n");
+    int i;
+    for(i = 0; i < argc; i++)
+     {
+      printf("%3d: %s\r\n", i, argv[i]);
+     }
+
     
     return 0;
  }
